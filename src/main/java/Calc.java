b455045ae257ad043de03a84ae055788bb1966ac
Maksim Uskov <MUskov87@mail.ru>
2021-11-05 00:18:27 +0400

    Fix solution

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,16 +1,52 @@
 import java.util.Scanner;
 
-//Scanner - Этот класс пригодится, для считывания данных, которые пользователь вводит.
-//Его функциональность очень проста. Он, словно настоящий сканер, считывает данные из источника, который ты для него укажешь.
+//Scanner - Этот класс, для считывания данных, которые пользователь вводит.
+//Его функциональность простая. Имулирует сканер, считывает данные , которые ты для него укажешь.
+// action = args [0] - сложение, вычитание и тд
 
 public class Calc {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);// анализируем входной поток данных
         System.out.println("Введите первое число");
-        int a = scanner.nextInt();// присваиваем первое введеное значение для переменной "a"
+        double a = scanner.nextDouble();// присваиваем первое введеное значение для переменной "a"
         System.out.println("Введите второе число");
-        int b = scanner.nextInt();// присваиваем первое введеное значение для переменной "b"
-        int c = a - b;
-        System.out.println("Ответ: " + c);
+        double b = scanner.nextDouble();// присваиваем первое введеное значение для переменной "b"
+        String action = args [0];
+
+        if(args[0].equals("+")) // сложение
+        {
+            System.out.println("Ответ: "+ (a+b));
+        }
+
+        if(args[0].equals("-")) //вычитание
+        {
+            System.out.println("Ответ: "+ (a-b));
+        }
+
+        if(args[0].equals("*")) //умножение
+        {
+            System.out.println("Ответ: "+ (a * b));
+        }
+
+        if(args[0].equals("/")) //деление
+        {
+            System.out.println("Ответ: "+ (a / b));
+        }
+
+        if(args[0].equals ("/") && b == 0)  // деление на ноль
+        {
+            System.out.println ("Деление на ноль невозможно");
+        }
+
+        if(args[0].equals("%")) //остаток от деления
+        {
+            System.out.println("Ответ: "+ (a / b));
+        }
+
+        if(args[0].equals("^")) //возведение в степень
+        {
+            System.out.println("Ответ: " + (Math.pow(a,b)));
+        }
+
     }
 }
